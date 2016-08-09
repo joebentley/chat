320d853bab46ff1f925e713d3e6606cec2d81cde
Joe Bentley <joebentley10@gmail.com>
2016-08-09 21:30:37 +0100

    Added README and some initial stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+# chat :crystal_ball:
+
+Simple TCP chat written in Node.JS :tada:
+
+Written as a hobby project with :coffee: by Joe Bentley
+
+### TODO
+
+* Add [emoji](https://www.npmjs.com/package/node-emoji) support
--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const net = require('net')
 const PORT_NUMBER = 3000
 
 net.createServer(function (socket) {
-  socket.write('hello world')
+  socket.setEncoding('utf8')
+
+  socket.on('data', function (data) {
+    socket.write(data.toString())
+  })
+
+  socket.on('error', function (err) {
+    console.error(err)
+  })
 }).listen(PORT_NUMBER)
 
-console.log(`Listening on ${PORT_NUMBER}...`)
+console.log(`Listening on port ${PORT_NUMBER}...`)
